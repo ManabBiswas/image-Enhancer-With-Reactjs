56ffe7627b558f74b8995c5ead49ea91480cf869
Manab Biswas <manabbiswas108108+github@gmail.com>
2025-03-31 01:45:26 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: ['@rollup/rollup-linux-x64-gnu'], // Exclude from bundling if required
+    },
+  }
 })
